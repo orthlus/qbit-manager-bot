9acfd4109d5a627a21443aa26379f45afd3a903e
ort <orthodoxplus@ya.ru>
2024-08-16 02:57:59 +0300

    init

---
+++ src/main/java/art/aelaort/AeQbitManagerBotApplication.java
@@ -0,0 +1,13 @@
+package art.aelaort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeQbitManagerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeQbitManagerBotApplication.class, args);
+	}
+
+}
